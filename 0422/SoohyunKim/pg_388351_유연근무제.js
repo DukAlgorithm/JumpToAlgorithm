fe029b7a9ba893387916302860fde4c396a13cee
soohyun <gamjatan9@gmail.com>
2025-04-22 13:12:24 +0900

    0422

--- 0422/SoohyunKim/pg_388351_유연근무제.js
+++ 0422/SoohyunKim/pg_388351_유연근무제.js
@@ -1,48 +1,26 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const T = +input[0];
-for (let t = 1; t <= T; t++) {
-  const [x, y] = input[t].split(' ').map(Number);
-  const d = y - x;
-
-  let n = 1;
-  while (true) {
-    if (d <= n * (n + 1)) {
-      if (d <= n * n) {
-        console.log(2 * n - 1);
-      } else {
-        console.log(2 * n);
+function solution(schedules, timelogs, startday) {
+  let answer = 0;
+  // 주말
+  let sat = 6 - startday;
+  const sun = sat + 1;
+  if(startday === 7) sat = 6;
+  
+  
+  for(let i = 0; i < schedules.length; i++) {
+      let cnt = 0;
+      let maxT = schedules[i] + 10; // 출근 가능한 최대 시간
+      if(maxT % 100 >= 60) { // 분 조정
+          maxT = maxT + 100 - 60;
       }
-      break;
-    }
-    n++;
-  }
-}
-
-/* 런타임에러, 테스트코드 통과
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const T = +input[0];
-for(let t = 1; t <= T; t++){
-  const [x, y] = input[t].split(' ').map(Number);
-  const target = y - 1;
-  let min = Infinity;
-
-  function dfs(cur, k, cnt) {
-    if (cur > target) return;
-    if (cur === target) {
-      min = Math.min(min, cnt + 1);
-      return;
-    }
-
-    // 현재 속도 ±1, 또는 유지 가능
-    for (let next of [k - 1, k, k + 1]) {
-      if (next > 0) {
-        dfs(cur + next, next, cnt + 1);
+      
+      for(let day = 0; day < 7; day++){
+          if(day === sat || day === sun) continue; // 주말 지나침
+          
+          let curT = timelogs[i][day];
+          if(curT > maxT) break;            
+          else cnt++;
       }
-    }
+      if(cnt === 5) answer++;
   }
-
-  dfs(x, 0, 0);
-  console.log(min);
+  return answer;
 }
-*/
